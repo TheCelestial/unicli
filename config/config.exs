d373ee923eaf33bde11b06b2bca0d7e286041d33
TheCelestial <TheCelestial@users.noreply.github.com>
2020-02-12 12:40:49 +0900

    Update config.exs

--- config/config.exs
+++ config/config.exs
@@ -4,6 +4,6 @@ config :elixir, ansi_enabled: true
 
 config :unicli, UniCLI.Controller,
   profile: {:system, "UNIFI_PROFILE", "default"},
-  host: {:system, "UNIFI_HOST", ""},
-  username: {:system, "UNIFI_USERNAME", ""},
-  password: {:system, "UNIFI_PASSWORD", ""}
+  host: {:system, "UNIFI_HOST", "https://10.9.6.8:8443"},
+  username: {:system, "UNIFI_USERNAME", "manager"},
+  password: {:system, "UNIFI_PASSWORD", "fr0ggy"}
